ba76df321e15e3adccc897bde32708dcdbee7c32
Askingex <luis_alejandrosanchez94@outlook.es>
2021-03-25 19:25:53 -0500

    server listo

---
+++ app.js
@@ -0,0 +1,7 @@
+import { } from 'dotenv/config.js';
+import {Server} from './models/Server.js';
+
+const server = new Server
+
+
+server.listen();
---
+++ index.js
@@ -0,0 +1,38 @@
+
+// import express from 'express';
+
+// var app = express();
+
+// // respond with "hello world" when a GET request is made to the homepage
+
+
+
+// app.get('/hola', function (req, res) {
+//   res.json({ mensaje: "sirviendo get" });
+// });
+
+
+// app.get('/articulos', function (req, res) {
+//   const { codigo, nombre } = req.query;
+//   res.json({ mensaje: "listado de articulos", nombre });
+// });
+
+
+// app.put('/articulos', function (req, res) {
+//   res.json({ mensaje: "sirviendo put" });
+// });
+// app.post('/articulos', function (req, res) {
+//   console.log(req.body);
+//   res.json({ msg: "sirviendo post", nombres });
+// });
+// app.delete('/articulos', function (req, res) {
+//   res.json({ mensaje: "sirviendo delete" });
+// });
+
+
+// const PORT = 3000;
+
+// app.listen(PORT, () => {
+//   console.log(`SERVIDOR CORRIENDO EN EL PUERTO ${PORT}`);
+// })
+
